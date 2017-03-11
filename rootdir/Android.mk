a78e6de402b70fc6de1f1f8b9adc7da0aa8f787c
Sanjeet990 <sanjeet.pathak990@gmail.com>
2017-03-11 17:45:26 +0000

    Volte Support

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,10 +3,10 @@ LOCAL_PATH:= $(call my-dir)
 # Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE       := init.qti.ims.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_SRC_FILES    := etc/init.qti.ims.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
