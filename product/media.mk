9f8fcccaa139ae62eaf8b4a3f9ddf6fcdc207e13
Sanjeet990 <sanjeet.pathak990@gmail.com>
2017-03-12 15:56:36 +0000

    Removed boringssl

--- product/media.mk
+++ product/media.mk
@@ -2,6 +2,3 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 	
-# Widevine DRM symbol, boringssl-compat subset
- PRODUCT_PACKAGES += \
-     libshim_boringssl
